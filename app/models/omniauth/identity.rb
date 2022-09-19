ef4e7b3e99f4e0c84b26da505ba2feecee5f8c5c
Rahoul Baruah <rahoulb@echodek.co>
2022-09-19 15:43:40 +0100

    Corrected Identity

--- app/models/omniauth/identity.rb
+++ app/models/omniauth/identity.rb
@@ -1,5 +1,3 @@
-require "omniauth-identity"
-
 class Omniauth::Identity < OmniAuth::Identity::Models::ActiveRecord
   belongs_to :authenticatable, polymorphic: true
   validates :email, presence: true
